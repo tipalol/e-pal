32930b55d4ed05f1ebcac2c8f735da244b1ca1d2
Sorokin Dmitriy <tipalol2@gmail.com>
2024-10-27 19:03:32 +0300

    add HttpContextAccessor to services

--- Epal.Application/Configuration/ApplicationConfiguration.cs
+++ Epal.Application/Configuration/ApplicationConfiguration.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 using Epal.Application.Interfaces;
 using Epal.Application.Services;
 using FluentValidation;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +17,8 @@ public static class ApplicationConfiguration
     public static IServiceCollection ConfigureApplication(this IServiceCollection services)
     {
         services.AddMediatR(b => b.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped<IPasswordService, PasswordService>();
         services.AddScoped<IVerificationService, VerificationService>();
         services.AddScoped<IUserService, UserService>();
