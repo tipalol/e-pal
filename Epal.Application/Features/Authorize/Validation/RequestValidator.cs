c5e79d31dab7529b7fa747b661adb3c18c851629
Hovva <35590736+Hovva@users.noreply.github.com>
2024-10-27 17:50:18 +0300

    validation

---
+++ Epal.Application/Features/Authorize/Validation/RequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Epal.Application.Features.Authorize.Validation;
+
+public class RequestValidator : AbstractValidator<AuthorizeRequest>
+{
+    public RequestValidator()
+    {
+        RuleFor(x => x.Login)
+            .NotEmpty()
+            .WithMessage("Имя пользователя не может быть пустым")
+            .MinimumLength(4)
+            .WithMessage("Слишком короткое имя пользователя")
+            .MaximumLength(30)
+            .WithMessage("Слишком длинное имя пользователя")
+            .Matches(@"^[a-zA-Z0-9_.-]+$")
+            .WithMessage("Имя пользователя может содержать только буквы, цифры, дефисы, точки и нижнее подчеркивание.");
+    }
+}
---
+++ Epal.Application/Features/Profiles/Validations/RequsetValidator.cs
@@ -0,0 +1,22 @@
+﻿using Epal.Application.Features.Profiles.Models;
+using FluentValidation;
+
+namespace Epal.Application.Features.Profiles.Validations;
+
+public class RequestValidator : AbstractValidator<ProfileModel>
+{
+
+    public RequestValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty()
+                .WithMessage("Имя пользователя не может быть пустым")
+            .MinimumLength(4)
+                .WithMessage("Слишком короткое имя пользователя")
+            .MaximumLength(30)
+                .WithMessage("Слишком длинное имя пользователя")
+            .Matches(@"^[a-zA-Z0-9_.-]+$")
+                .WithMessage("Имя пользователя может содержать только буквы, цифры, дефисы, точки и нижнее подчеркивание.");
+    }
+    
+}
