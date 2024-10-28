7143180d78473f4e5739b39e5601bb61828819cd
Dima Sensei <sorokin_dy@tochka.com>
2024-10-28 07:59:40 +0500

    first catalog method

--- Epal.Api/Controllers/CatalogController.cs
+++ Epal.Api/Controllers/CatalogController.cs
@@ -1,9 +1,11 @@
 using Epal.Api.Controllers.Base;
+using Epal.Application.Features.Catalog.ServiceTypes;
 using MediatR;
 
 namespace Epal.Api.Controllers;
 
 public class CatalogController(ISender sender) : RestController(sender)
 {
-    
+    public async Task GetServiceTypes()
+        => await Sender.Send(new ServiceTypesCatalogRequest());
 }
