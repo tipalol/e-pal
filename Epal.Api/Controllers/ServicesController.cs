63988cca184a4fb69aaeffda72be09882045feec
Dima Sensei <sorokin_dy@tochka.com>
2024-10-28 07:33:18 +0500

    added get served categories method

--- Epal.Api/Controllers/ServicesController.cs
+++ Epal.Api/Controllers/ServicesController.cs
@@ -4,6 +4,8 @@ using Epal.Application.Features.Services.Add;
 using Epal.Application.Features.Services.Add.Models;
 using Epal.Application.Features.Services.Get;
 using Epal.Application.Features.Services.Get.Models;
+using Epal.Application.Features.Services.GetTypes;
+using Epal.Application.Features.Services.GetTypes.Models;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,11 +14,15 @@ namespace Epal.Api.Controllers;
 
 public class ServicesController(ISender sender) : RestController(sender)
 {
+    [HttpGet("{profileId:guid}/categories")]
+    public async Task<Result<IEnumerable<ServiceTypeListView>>> GetTypes([FromRoute(Name = "profileId")] Guid profileId)
+        => await Sender.Send(new GetServiceTypesRequest(profileId));
+
     [HttpGet("{profileId:guid}/category/{categoryId:guid}")]
     public async Task<Result<IEnumerable<ServiceListView>>> Get([FromRoute(Name = "profileId")] Guid profileId, [FromRoute(Name = "categoryId")] Guid categoryId)
         => await Sender.Send(new GetServicesRequest(profileId, categoryId));
 
     [HttpPost, Authorize]
-    public async Task<Result> AddService(ServiceDto serviceDto)
+    public async Task<Result> Add(ServiceDto serviceDto)
         => await Sender.Send(new CreateServiceRequest(serviceDto));
 }
